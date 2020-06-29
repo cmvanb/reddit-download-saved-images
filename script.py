fe50cd2679c83743b222cc86f7711edc26431d66
Casper <cmvanb@gmail.com>
2020-06-29 02:30:57 +0200

    make authenticated request

--- script.py
+++ script.py
@@ -16,14 +16,28 @@ def download_saved_images():
     app_token  = config['DEFAULT']['app_token']
     app_secret = config['DEFAULT']['app_secret']
 
+    user_agent = 'DownloadImages script'
+
     # ---- Get access token ---------------------------------------------------
 
-    url = 'https://www.reddit.com/api/v1/access_token'
+    url         = 'https://www.reddit.com/api/v1/access_token'
     client_auth = requests.auth.HTTPBasicAuth(app_token, app_secret)
-    payload = { 'grant_type': 'password', 'username': username, 'password': password }
-    headers = { 'User-Agent': 'DownloadImages script' }
+    payload     = { 'grant_type': 'password', 'username': username, 'password': password }
+    headers     = { 'User-Agent': user_agent }
 
     response = requests.post(url, auth=client_auth, data=payload, headers=headers)
+
+    data         = response.json()
+    access_token = data['access_token']
+    token_type   = data['token_type']
+
+    authorization = token_type + ' ' + access_token
+
+    # ---- Get user data ------------------------------------------------------
+
+    url      = 'https://oauth.reddit.com/api/v1/me'
+    headers  = { 'Authorization': authorization, 'User-Agent': user_agent }
+    response = requests.get(url, headers=headers)
 
     data = response.json()
     print(data)
