6641399e771ef8a14e6047ecf30a28fa3c151ee9
Casper <cmvanb@gmail.com>
2020-06-29 03:59:30 +0200

    filter posts by whether they contain an image

--- script.py
+++ script.py
@@ -1,4 +1,30 @@
 # -----------------------------------------------------------------------------
+# Globals
+# -----------------------------------------------------------------------------
+
+IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'png']
+
+# -----------------------------------------------------------------------------
+# Utility
+# -----------------------------------------------------------------------------
+
+def index_of(value, list):
+    try:
+        return list.index(value)
+    except ValueError:
+        return -1
+
+def contains(value, list):
+    return index_of(value, list) != -1
+
+def is_url_image(url):
+    try:
+        file_extension = url.rpartition('.')[-1]
+        return contains(file_extension, IMAGE_EXTENSIONS)
+    except:
+        return False
+
+# -----------------------------------------------------------------------------
 # Download user's saved images
 # -----------------------------------------------------------------------------
 def download_saved_images():
@@ -18,6 +44,9 @@ def download_saved_images():
     app_token  = config['DEFAULT']['app_token']
     app_secret = config['DEFAULT']['app_secret']
 
+    config.read('input/filters.ini')
+    subreddit  = config['DEFAULT']['subreddit']
+
     user_agent = 'DownloadImages script'
 
     # ---- Get access token ---------------------------------------------------
@@ -29,9 +58,14 @@ def download_saved_images():
 
     response = requests.post(url, auth=client_auth, data=payload, headers=headers)
 
-    data         = response.json()
-    access_token = data['access_token']
-    token_type   = data['token_type']
+    if response.status_code != 200:
+        print('ERROR: response did not return 200 OK')
+        print(response)
+        exit()
+
+    json_response = response.json()
+    access_token  = json_response['access_token']
+    token_type    = json_response['token_type']
 
     authorization = token_type + ' ' + access_token
 
@@ -41,12 +75,24 @@ def download_saved_images():
     headers  = { 'Authorization': authorization, 'User-Agent': user_agent }
     response = requests.get(url, headers=headers)
 
-    data = response.json()
+    json_response = response.json()
+    posts         = json_response['data']['children']
 
-    output_file = 'output/saved_posts.json'
-    data_unicode = unicode(json.dumps(data))
-    with io.open(output_file, 'w', encoding='utf-8') as f:
-        f.write(data_unicode)
+    # ---- Filter posts -------------------------------------------------------
+
+    filtered_posts = filter(lambda post: post['data']['subreddit'] == subreddit, posts)
+
+    for post in filtered_posts:
+        url = post['data']['url']
+        if is_url_image(url):
+            print(url + ' is an image!')
+            # TODO: download image
+
+    # output_file   = 'output/saved_posts.json'
+    # json_unicode  = unicode(json.dumps(json_response))
+
+    # with io.open(output_file, 'w', encoding='utf-8') as f:
+    #     f.write(json_unicode)
 
 # -----------------------------------------------------------------------------
 # Entry point
