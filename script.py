7a21717e8ac1d4f3cfdf770a46c16e6e1cd3bb1a
Casper <cmvanb@gmail.com>
2020-07-09 04:11:00 +0200

    retrieve all saved posts

--- script.py
+++ script.py
@@ -65,10 +65,10 @@ def download_saved_images():
 
     url         = 'https://www.reddit.com/api/v1/access_token'
     client_auth = requests.auth.HTTPBasicAuth(app_token, app_secret)
-    payload     = { 'grant_type': 'password', 'username': username, 'password': password }
+    data        = { 'grant_type': 'password', 'username': username, 'password': password }
     headers     = { 'User-Agent': user_agent }
 
-    response = requests.post(url, auth=client_auth, data=payload, headers=headers)
+    response = requests.post(url, auth=client_auth, data=data, headers=headers)
 
     if response.status_code != 200:
         print('ERROR: response did not return 200 OK')
@@ -82,40 +82,69 @@ def download_saved_images():
     authorization = token_type + ' ' + access_token
 
     # ---- Get user's saved posts ----------------------------------------------
-    # TODO: Currently, this only retrieves the most recent 25 saved posts.
-    # The script should retrieve all saved image posts, optionally filter them
-    # (e.g. by image size, subreddit), download the images, and then optionally
-    # unsave the posts.
-    # https://www.reddit.com/dev/api
+    # TODO: Refactor initial request into loop
 
     url      = 'https://oauth.reddit.com/user/' + username + '/saved'
     headers  = { 'Authorization': authorization, 'User-Agent': user_agent }
     response = requests.get(url, headers=headers)
 
     json_response = response.json()
-    posts         = json_response['data']['children']
+    all_posts     = json_response['data']['children']
+    after         = json_response['data']['after']
+
+    print('Retrieving saved posts for ' + username + '...')
+
+    while after is not None:
+        params   = { 'after': after }
+        response = requests.get(url, headers=headers, params=params)
+
+        if response.status_code != 200:
+            print('ERROR: response did not return 200 OK')
+            print(response)
+            exit()
+
+        json_response = response.json()
+        posts         = json_response['data']['children']
+        after         = json_response['data']['after']
+
+        all_posts     = all_posts + posts
+
+    print(' ...found ' + str(len(all_posts)) + '.')
 
     # ---- Filter posts --------------------------------------------------------
+    # TODO: Filter by image size
 
-    filtered_posts = filter(lambda post: post['data']['subreddit'] == subreddit, posts)
+    print('Filtering for images...')
+
+    filtered_posts = filter(lambda post: post['data']['subreddit'] == subreddit, all_posts)
     target_urls = {}
 
     for post in filtered_posts:
-        title = post['data']['title']
-        url   = post['data']['url']
+        title   = post['data']['title']
+        url     = post['data']['url']
+
         if is_url_image(url):
-            print(url + ' is an image!')
             target_urls[title] = url
 
+    print(' ...found ' + str(len(target_urls)) + '.')
+
     # ---- Download image posts ------------------------------------------------
+    # TODO: Normalize file names
+
+    print('Downloading images...')
 
     for title, url in target_urls.iteritems():
         try:
             file_extension = get_file_extension(url)
             file_name      = title + '.' + file_extension
+            print(' -> ' + url)
             download_image(url, 'output/' + file_name)
         except:
             continue
+
+    print(' ...done.')
+
+    # TODO: Unsave post (optional)
 
 # ------------------------------------------------------------------------------
 # Entry point
